ffcf3fc70709335ef99547822cdd6bd3421214c1
Kimbumsoo99 <show7441@naver.com>
2023-03-15 04:10:52 +0900

    세션 및 템플릿 적용 서버

--- tempPug/src/middlewares.js
+++ tempPug/src/middlewares.js
@@ -1,6 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.user = req.session.user;
-  console.log(res.locals);
   next();
 };
--- tempPug/src/server.js
+++ tempPug/src/server.js
@@ -5,12 +5,15 @@ import userRouter from "./routers/userRouter";
 import boardRouter from "./routers/boardRouter";
 import session from "express-session";
 import { localsMiddleware } from "./middlewares";
+import favicon from "serve-favicon";
+import path from "path";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
+app.use(favicon(path.join(__dirname, "public", "1.png")));
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger); //morgan GET, path, status code, 응답 시간 등 request에 대한 정보를 준다
@@ -26,7 +29,6 @@ app.use(
 app.use((req, res, next) => {
   //res.locals.user = "김범수"
   req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
     next();
   });
 });
