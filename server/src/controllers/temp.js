76d2c38b1759c6bca6b6bc3f20f08c411a6b9901
Kimbumsoo99 <show7441@naver.com>
2023-03-30 17:13:13 +0900

    수정

--- server/src/controllers/temp.js
+++ server/src/controllers/temp.js
@@ -105,3 +105,47 @@ export const host2 = (request,res)=>{
     });
   });
 }
+
+export const host3 = (request,res)=>{
+    const username = "administrator@vsphere.local";
+    const password = "123Qwer!";
+    const host = "192.168.0.102";
+  
+   const data = JSON.stringify({});
+  
+    const options = {
+      hostname: host,
+      port: 443,
+      path: "/api/session",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+        Authorization:
+          "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+      },
+      rejectUnauthorized: false,
+    };
+  
+    const req = https.request(options, (res) => {
+      let responseBody = "";
+    
+      res.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+  
+  
+      res.on("end", () => {
+        const jsonResponse = JSON.parse(responseBody);
+        console.log("나는 json",jsonResponse);
+      });
+    });
+  
+    req.on("error", (error) => {
+      console.error(error);
+    });
+    console.log("나는 data",jsonResponse);
+    req.write(data);
+    req.end();
+  
+}
--- server/src/routers/rootRouters.js
+++ server/src/routers/rootRouters.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { home, host, host2 } from "../controllers/temp";
+import { home, host, host2, host3 } from "../controllers/temp";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.get("/host", host);
 rootRouter.get("/host2",host2)
+rootRouter.get("/host2",host3)
+
 
 export default rootRouter;
