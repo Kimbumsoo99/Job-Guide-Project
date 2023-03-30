e93fa128b9f894a4a1ff139ea93a576ac1a70fa1
Kimbumsoo99 <show7441@naver.com>
2023-03-30 16:35:58 +0900

    host2 session Id get

--- server/src/controllers/temp.js
+++ server/src/controllers/temp.js
@@ -36,33 +36,42 @@ export const host = (req, res) => {
   });
 };
 
-export const host2 = async(req,res)=>{
-  const username = "administrator";
+export const host2 = (request,res)=>{
+  const username = "administrator@vsphere.local";
   const password = "123Qwer!";
   const host = "192.168.0.102";
-  const vmwareHeaders = {
-    "Content-Type": "application/json",
-    Authorization:
-      "Basic " + Buffer.from(username + ":" + password).toString("base64"),
-  };
+  const data = JSON.stringify({});
 
   const options = {
-    headers: vmwareHeaders,
-    rejectUnauthorized: false
+    hostname: host,
+    port: 443,
+    path: "/api/session",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+      Authorization:
+        "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+    },
+    rejectUnauthorized: false,
   };
-  https.get(`https://${host}/rest/vcenter/vm`, options, (response) => {
-    let data = "";
-
-    response.on("data", (chunk) => {
-      data += chunk;
+  const req = https.request(options, (res) => {
+    let responseBody = "";
+  
+    res.on("data", (chunk) => {
+      responseBody += chunk;
     });
-
-    response.on("end", () => {
-      const vms = JSON.parse(data);
-      console.log("모든 가상 머신 정보:");
-      console.log(vms);
-      return res.send(vms);
-
+  
+    res.on("end", () => {
+      const jsonResponse = JSON.parse(responseBody);
+      console.log(jsonResponse);
     });
   });
+  
+  req.on("error", (error) => {
+    console.error(error);
+  });
+  
+  req.write(data);
+  req.end();
 }
