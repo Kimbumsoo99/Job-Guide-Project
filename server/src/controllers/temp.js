f88a109af985217461ded11a0d5fcd43f59a7768
Kimbumsoo99 <show7441@naver.com>
2023-03-30 15:21:28 +0900

    수정

--- server/src/controllers/temp.js
+++ server/src/controllers/temp.js
@@ -30,3 +30,38 @@ export const host = (req, res) => {
     });
   });
 };
+
+export const host2 = async(req,res)=>{
+  const username = "administrator@vsphere.local";
+  const password = "123Qwer!";
+  const host = "192.168.0.102";
+
+  const VcenterClient = require('vmware-web-automation-sdk').VcenterClient;
+
+  const client = new VcenterClient(host);
+  await client.auth.login(username, password);
+  const sessionId = client.getSessionId();
+  const vmwareHeaders = {
+    "Content-Type": "application/json",
+    "vmware-api-session-id": sessionId,
+  };
+
+  https.get(
+    `https://vcenter.example.com/rest/vcenter/vm/${vmId}`,
+    { headers: vmwareHeaders },
+    (response) => {
+      let data = "";
+  
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+  
+      response.on("end", () => {
+        const vms = JSON.parse(data);
+        console.log("모든 가상 머신 정보:");
+        console.log(vms);
+        return res.send(vms);
+      });
+    }
+  );
+}
--- server/src/routers/rootRouters.js
+++ server/src/routers/rootRouters.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { home, host } from "../controllers/temp";
+import { home, host, host2 } from "../controllers/temp";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.get("/host", host);
+rootRouter.get("/host2",host2)
 
 export default rootRouter;
