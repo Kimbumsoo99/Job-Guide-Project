90d97aeeafb3b56687cb72acafe28e4c7b6edbb6
Kimbumsoo99 <show7441@naver.com>
2023-04-04 16:45:38 +0900

    수정

--- server/src/controllers/temp.js
+++ server/src/controllers/temp.js
@@ -1,10 +1,10 @@
+export const home = (req, res) => res.render("home");
+
 const https = require("https");
 
 const username = "administrator@vsphere.local";
 const password = "123Qwer!";
 const hostIP = "192.168.0.102";
-
-export const home = (req, res) => res.render("home");
 
 const getSessionId = async () => {
   const data = JSON.stringify({});
@@ -51,6 +51,59 @@ const getSessionId = async () => {
   sessionIdJson = res.value;
   console.log("session ID:", sessionIdJson);
   return sessionIdJson;
+};
+
+export const getVmAfterHostCPU = async (req, res) => {
+  try {
+    const sessionIdJson = await getSessionId();    //sessionIdJson == vmware-api-session-id
+    console.log("SessionID GET After: " +sessionIdJson); // 이부분에서 가져온 Session ID를 확인
+    console.log("Error SessionID: " +sessionIdJson.value); // 이 부분은 에러
+    
+    const vmwareHeaders = {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+      "vmware-api-session-id": sessionIdJson,
+    };
+
+    const options = {
+      headers: vmwareHeaders,
+      rejectUnauthorized: false,
+    };
+
+    https.get(`https://${hostIP}/rest/vcenter/host`, options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        const vms = JSON.parse(data);
+        console.log("모든 가상 머신 정보:");
+        console.log(vms.value[0].vm);
+        const vmId=vms.value[0].vm;
+        
+        return https.get(`https://${hostIP}/rest/vcenter/vm/${vmId}`, options, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+          data += chunk;
+        });
+      
+        response.on("end", () => {
+          const cpuInfo = JSON.parse(data);
+          console.log("CPU 사용량 정보:");
+          console.log(cpuInfo);
+          return res.send(cpuInfo);
+          });
+        });
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error");
+  }
 };
 
 // GPT https로 Session 얻고 바로 VM 정보
@@ -179,57 +232,4 @@ export const preGetSessionId = (request, res) => {
   console.log("나는 data", jsonResponse);
   req.write(data);
   req.end();
-};
-
-export const getVmAfterHostCPU = async (req, res) => {
-  try {
-    const sessionIdJson = await getSessionId();    //sessionIdJson == vmware-api-session-id
-    console.log("SessionID GET After: " +sessionIdJson); // 이부분에서 가져온 Session ID를 확인
-    console.log("Error SessionID: " +sessionIdJson.value); // 이 부분은 에러
-    
-    const vmwareHeaders = {
-      "Content-Type": "application/json",
-      Authorization:
-        "Basic " + Buffer.from(username + ":" + password).toString("base64"),
-      "vmware-api-session-id": sessionIdJson,
-    };
-
-    const options = {
-      headers: vmwareHeaders,
-      rejectUnauthorized: false,
-    };
-
-    https.get(`https://${hostIP}/rest/vcenter/host`, options, (response) => {
-      let data = "";
-
-      response.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      response.on("end", () => {
-        const vms = JSON.parse(data);
-        console.log("모든 가상 머신 정보:");
-        console.log(vms.value[0].vm);
-        const vmId=vms.value[0].vm;
-        
-        return https.get(`https://${hostIP}/rest/vcenter/vm/${vmId}`, options, (response) => {
-        let data = "";
-
-        response.on("data", (chunk) => {
-          data += chunk;
-        });
-      
-        response.on("end", () => {
-          const cpuInfo = JSON.parse(data);
-          console.log("CPU 사용량 정보:");
-          console.log(cpuInfo);
-          return res.send(cpuInfo);
-          });
-        });
-      });
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Error");
-  }
 };
