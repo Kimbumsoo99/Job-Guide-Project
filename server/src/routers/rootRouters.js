625d182ed9a5074366f018c503351abc8b15ad65
Kimbumsoo99 <show7441@naver.com>
2023-04-06 18:57:36 +0900

    수정 전

--- server/src/routers/rootRouters.js
+++ server/src/routers/rootRouters.js
@@ -14,13 +14,6 @@ import { noBasicAPI } from "../controllers/noBasicAPI";
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.get("/session/pre", preGetSessionId);
-rootRouter.get("/session/api", hostSesssionCreate);
-rootRouter.get("/host/nomal", hostGetSessionGetVM);
-rootRouter.get("/host/axios", hostAxios);
-// dif/ip 대신 host로 변경
 rootRouter.get("/dif/ip", getVmAfterHostCPU);
-rootRouter.get("/dif/username", getSessionDifUsername);
-rootRouter.get("/basic/no", noBasicAPI);
 
 export default rootRouter;
