a2133f8c411c38f28e5fe76989fa0e1ce2d227a7
Kimbumsoo99 <show7441@naver.com>
2023-04-11 07:19:36 +0900

    로그인 기능 추가

--- login/src/controllers/userController.js
+++ login/src/controllers/userController.js
@@ -1,11 +1,60 @@
+import User from "../models/User";
+import bcrypt from "bcrypt"; // 비밀번호 암호화
+
 export const home = (req, res) => res.render("home");
 
 export const getLogin = (req, res) => {
   return res.render("login");
 };
 
-export const postLogin = (req, res) => {};
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    console.log("postLogin 유저 존재 X");
+    return res.status(400).render("login");
+  }
+
+  // 비번 확인
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    console.log("postLogin 비밀번호 실패");
+    return res.status(400).render("login");
+  }
+
+  //세션 추가
+  return res.redirect("/");
+};
 
 export const getJoin = (req, res) => {
   return res.render("join");
+};
+
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, password2 } = req.body;
+  if (password !== password2) {
+    console.log("비밀번호 오류");
+    return res.status(400).render("join");
+  }
+  const user = await User.findOne({ $or: [{ username }, { email }] });
+  console.log("유저 확인");
+  console.log(user);
+  if (user) {
+    console.log("postJoin 유저 중복");
+    return res.status(400).render("login");
+  }
+
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+    });
+    return res.redirect("login");
+  } catch (error) {
+    console.log("postJoin 유저 생성 오류");
+    return res.status(400).render("join");
+  }
 };
--- login/src/models/User.js
+++ login/src/models/User.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+});
+
+// Mongo 6.0에 save 없어짐 insertOne
+UserSchema.pre("insertOne", async function () {
+  //비밀번호가 수정될때만 미들웨어 동작
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
