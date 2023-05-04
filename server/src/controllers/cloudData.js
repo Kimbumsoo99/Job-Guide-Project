38f9b8300fb5762eaaa56c6b79a17dfe2ffd28c2
Kimbumsoo99 <show7441@naver.com>
2023-05-04 17:58:12 +0900

    오류 3

--- server/src/controllers/cloudData.js
+++ server/src/controllers/cloudData.js
@@ -1,4 +1,4 @@
-import User from "../../../temp_server/src/models/User";
+import User from "../models/User";
 import { getSessionId } from "./headerGet";
 import { getVMList } from "./vmController";
 import {
--- server/src/controllers/vmController.js
+++ server/src/controllers/vmController.js
@@ -1,5 +1,5 @@
 import { getSessionId } from "./headerGet";
-//import User from "../models/User";
+import User from "../models/User";
 
 const https = require("https");
 
