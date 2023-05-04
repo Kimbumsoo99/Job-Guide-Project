0d0fa1be932aea1eb09481f019e98f34182eeb3e
Kimbumsoo99 <show7441@naver.com>
2023-05-04 15:51:06 +0900

    host 페이지 테스트

--- temp_server/src/controllers/cloudData.js
+++ temp_server/src/controllers/cloudData.js
@@ -81,3 +81,56 @@ export const getCloudData = async (req, res) => {
     return res.redirect("/test/page");
   }
 };
+
+export const testGetHost = async (req, res) => {
+  console.log(req.query);
+
+  const {
+    session: {
+      user: { _id },
+    },
+  } = req;
+  const { vs_id, vs_pw, vs_ip } = req.query ? req.query : null;
+
+  if (vs_id && vs_pw && vs_ip) {
+    //쿼리 값 존재 시 -> 새롭게 등록
+    try {
+      //중복 여부 확인(IP 주소 중복 시)
+      const isDuplicated = await User.exists({
+        _id,
+        "vsphere.vs_ip": vs_ip,
+      });
+      if (isDuplicated) {
+        //IP주소가 중복 -> 같은 vCenter에 대한 값을 가져오는 것이므로 X
+        console.log("Duplicate data");
+        return redirect("/");
+      } else {
+        const sessionId = await getSessionId(vs_id, vs_pw, vs_ip);
+        const hostInfo = await getHost(sessionId);
+
+        const updatedUser = await User.findByIdAndUpdate(
+          _id,
+          {
+            $push: {
+              vsphere: {
+                vs_id,
+                vs_pw,
+                vs_ip,
+                info: { hostInfo },
+              },
+            },
+          },
+          { new: true } // 최근 업데이트 된 데이터로 변경
+        );
+        req.session.user = updatedUser;
+        return res.redirect("/test/page");
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Error");
+    }
+  } else {
+    // 쿼리 없으면 -> 그냥 기존 데이터 보여줌
+    return res.redirect("/test/page");
+  }
+};
--- temp_server/src/routers/vmRouter.js
+++ temp_server/src/routers/vmRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { getAddBasicInfo, postAddBasicInfo } from "../controllers/vmController";
-import { getCloudData } from "../controllers/cloudData";
+import {
+  getAddBasicInfo,
+  getHost,
+  postAddBasicInfo,
+} from "../controllers/vmController";
+import { getCloudData, testGetHost } from "../controllers/cloudData";
 import { protectorMiddleware } from "../middlewares";
 import { patchMemory, startPower, stopPower } from "../controllers/vmChangeSet";
 import { testGetData } from "../controllers/Test";
@@ -13,10 +17,14 @@ vmRouter
   .get(getAddBasicInfo)
   .post(postAddBasicInfo);
 
-//vmRouter.get("/data", getCloudData);
-vmRouter.get("/data", testGetData);
+vmRouter.get("/data", getCloudData);
+//vmRouter.get("/data", testGetData);
 vmRouter.get("/patch/memory", patchMemory);
 vmRouter.get("/stop/power", stopPower);
 vmRouter.get("/start/power", startPower);
+
+//
+//Test 코드
+vmRouter.get("/host", testGetHost);
 
 export default vmRouter;
