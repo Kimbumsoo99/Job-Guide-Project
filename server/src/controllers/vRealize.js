1ec08aa6e6bf361f6f70a7155c28a2e181766bc0
Kimbumsoo99 <show7441@naver.com>
2023-05-16 16:49:55 +0900

    CPU current 제거

--- server/src/controllers/vRealize.js
+++ server/src/controllers/vRealize.js
@@ -83,7 +83,7 @@ export const getRealCpuUsageV2 = async (req, res) => {
   const resourceId = await getRealResourcesV2();
   console.log("\ngetRealCpuUsageV2 복귀\n");
   console.log(token);
-  const url = `${baseUrl}/resources/${resourceId}/stats/latest?currentOnly=true&statKey=cpu|usage_average`;
+  const url = `${baseUrl}/resources/${resourceId}/stats/latest?statKey=cpu|usage_average`;
   const options = getOptions(token);
   const realCpuStats = await httpsGet(url, options);
   console.log(realCpuStats);
