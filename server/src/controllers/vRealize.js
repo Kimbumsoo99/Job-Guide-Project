d48a78c4e0e7910d6f6324cd24bcec26795d1ae9
Kimbumsoo99 <show7441@naver.com>
2023-05-16 17:03:40 +0900

    시도 1

--- server/src/controllers/vRealize.js
+++ server/src/controllers/vRealize.js
@@ -83,9 +83,10 @@ export const getRealCpuUsageV2 = async (req, res) => {
   const preTime = currentTime - 1000 * 60 * 60;
   console.log("\ngetRealCpuUsageV2 호출\n");
   const resourceId = await getRealResourcesV2();
+  console.log(resourceId);
   console.log("\ngetRealCpuUsageV2 복귀\n");
   console.log(token);
-  const url = `${baseUrl}/resources/${resourceId}/stats?begin=${preTime}&end=${currentTime}&statKey=cpu|usage_average`;
+  const url = `${baseUrl}/resources/${resourceId}/stats?statKey=cpu|usage_average`;
   const options = getOptions(token);
   const realCpuStats = await httpsGet(url, options);
   console.log(realCpuStats);
