ed1f31f1024d09aa9b3edf156343f419bf6a0d4e
Kimbumsoo99 <show7441@naver.com>
2023-05-30 16:42:01 +0900

    수정 3

--- temp_server/src/apis/vCenterAPI.js
+++ temp_server/src/apis/vCenterAPI.js
@@ -144,7 +144,7 @@ export const testCreateVM = async (req, res) => {
             placement: {
                 //여기는 파라미터
                 datastore: "datastore-48021",
-                folder: "group-h51031",
+                // folder: "group-h51031",
                 // folder: "group-v38002",
                 folder: "group-v35012",
                 // host: "esxi03.stz.local",
